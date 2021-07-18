982d9154ad5c19ec8320ed40900a6eaa3098fde8
kevintran <kevintran351996@gmail.com>
2021-07-18 23:12:08 +0700

    bai tap ve xem phim

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import BaiTapGioHangRedux from './BaiTapGioHangRedux/BaiTapGioHangRedux';
 import BaiTapGameXuatSacRedux from './BaiTapGameXuatSac/BaiTapGameXuatSacRedux';
 import BaiTapOanTuTi from './BaiTapOanTuTi/BaiTapOanTuTi';
+import BaiTapBookingTecKet from './BaiTapBookingTecket/BaiTapBookingTecKet';
 
 export default class App extends Component{
   render() {
     return (
       <div>
         {/* <BaiTapGioHangRedux /> */}
-        <BaiTapGameXuatSacRedux />
+        {/* <BaiTapGameXuatSacRedux /> */}
         {/* <BaiTapOanTuTi/> */}
+        <BaiTapBookingTecKet />
       </div>
     )
   }
